3a8e33404e94583493a1dead9179650f8cccac07
Harshak Chaudhari <80267529+harshak2001@users.noreply.github.com>
2023-12-22 10:16:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ const url = require("url");
 const { fileURLToPath } = require("url");
 const path = require("path");
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(filename);
 
 app.use(express.static(path.join(__dirname, "./client/build")));
 
